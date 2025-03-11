a97c4f3831077eb05f3508a4a5f432fe30b11eb4
Ben Cantrell <ben.cantrell@okstate.edu>
2025-03-11 13:49:26 -0500

    Initial commit

---
+++ src/main/java/com/example/JeopardyBackend/JeopardyBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.JeopardyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeopardyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeopardyBackendApplication.class, args);
+    }
+}
